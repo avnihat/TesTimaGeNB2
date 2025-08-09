f2b041886384e7268888d116c829af6d052a863e
avnihat <av.nihatbas@gmail.com>
2025-08-09 22:57:29 +0300

    Delete gps_checker.py

--- gps_checker.py
+++
@@ -1,8 +0,0 @@
-def gps_consistency(exif_dict):
-    # Basit bir örnek kural: Software alanı 'Snapchat' vb ise ve GPSInfo yoksa uyarı
-    software = str(exif_dict.get('Software', '')).lower()
-    has_gps = 'GPSInfo' in exif_dict
-    issues = []
-    if 'snapchat' in software and not has_gps:
-        issues.append('Software=Snapchat ama GPS bilgisi yok.')
-    return {'gps_uyari': issues or None}
